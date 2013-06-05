90d18a9b9ae351256af8e26188242af58970d5ea
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2013-06-05 13:19:33 -0400

    Removed unnecessary variable in SDL.

--- Sdl.cpp
+++ Sdl.cpp
@@ -7,7 +7,6 @@ UniqueSurface SCREEN { nullptr };
 
 void initialize() {
     SDL_Init( SDL_INIT_VIDEO );
-    SDL_Event event;
     std::atexit( &SDL_Quit );
 
     SCREEN.reset ( SDL_SetVideoMode( sim::MONITOR_PIXELS_PER_SCREEN_WIDTH,
