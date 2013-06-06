5c8386a99b3612d54dd786a28d60fa0116022464
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2013-06-06 16:38:07 -0400

    Added repeated keystrokes in SDL.

--- Sdl.cpp
+++ Sdl.cpp
@@ -10,6 +10,8 @@ void initialize() {
     std::atexit( &SDL_Quit );
 
     SDL_EnableUNICODE( 1 );
+    SDL_EnableKeyRepeat( SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL );
+
     SCREEN.reset ( SDL_SetVideoMode( sim::MONITOR_PIXELS_PER_SCREEN_WIDTH,
                                      sim::MONITOR_PIXELS_PER_SCREEN_HEIGHT,
                                      16,
