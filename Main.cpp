edc161b78dd66d1e205f553aa1df87d348d8f24d
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2013-07-09 14:43:02 -0400

    Added Boost.ProgramOptions and an option for little-endian encoding.

--- Main.cpp
+++ Main.cpp
@@ -5,17 +5,63 @@
 #include "Simulation/Processor.hpp"
 #include "Simulation/Monitor.hpp"
 
+#include <cstddef>
 #include <iostream>
+
+#include <boost/program_options.hpp>
 
 using namespace nebula;
 
 DEFINE_LOGGER( MAIN, "Main" )
 
-int main( int, char* argv[] ) {
+int main( int argc, char* argv[] ) {
+    namespace po = boost::program_options;
+
+    po::options_description visible;
+    visible.add_options()
+        ( "help,h", "Produce this message." )
+        ( "little-endian,e", "Assume little endian memory encoding." )
+        ;
+
+    po::options_description hidden;
+    hidden.add_options()
+        ( "memory-file", po::value<std::string>(), "The memory file initially loaded by the DCPU-16." )
+        ;
+
+    po::options_description desc;
+    desc.add( visible ).add( hidden );
+
+    po::positional_options_description pos;
+    pos.add( "memory-file", 1 );
+
+    po::variables_map vm {};
+
+    try {
+        po::store( po::command_line_parser( argc, argv ).
+                   options( desc ).positional( pos ).run(),
+                   vm );
+        po::notify( vm );
+    } catch ( po::error& err ) {
+        std::cerr << "nebula: " << err.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if ( vm.count( "help" ) || ! vm.count( "memory-file" ) ) {
+        std::cout << "nebula [options] memory-file" << std::endl;
+        std::cout << std::endl;
+        std::cout << visible << std::endl;
+        return EXIT_SUCCESS;
+    }
+
     logging::initialize( true, logging::Severity::info );
     sdl::initialize();
 
-    auto memory = Memory::fromFile( argv[1], 0x10000, ByteOrder::BigEndian );
+
+    auto byteOrder = vm.count( "little-endian" ) ? ByteOrder::LittleEndian : ByteOrder::BigEndian;
+
+    auto memory = Memory::fromFile( vm["memory-file"].as<std::string>(),
+                                    0x10000,
+                                    byteOrder );
     Computer computer { memory };
 
     Processor proc { computer };
