5c0eeb8df8a6810c2d7fbf2b291d3ff6da41c668
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2015-08-12 11:43:13 -0400

    Support precision clock on OS X

--- src/precision_clock_stub.c
+++ src/precision_clock_stub.c
@@ -3,14 +3,25 @@
 
 #include <time.h>
 
+#ifdef __MACH__
+#include <mach/mach_time.h>
+#endif
+
 #define NANOSECONDS_PER_SECOND 1000000000
 
 CAMLprim value nebula_precision_clock_get_time(void) {
+  long nanoseconds;
+
+#ifdef __MACH__
+  nanoseconds = mach_absolute_time();
+#else
   struct timespec ts;
 
   // Assume success.
   clock_gettime(CLOCK_REALTIME, &ts);
 
-  long nanoseconds = (NANOSECONDS_PER_SECOND * ts.tv_sec) + ts.tv_nsec;
+  nanoseconds = (NANOSECONDS_PER_SECOND * ts.tv_sec) + ts.tv_nsec;
+#endif
+
   return Val_long(nanoseconds);
 }
