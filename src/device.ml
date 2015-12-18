c0dca343bcb55306a7b535d23cd111dc2b714706
Jesse Haber-Kucharsky <jesse@haberkucharsky.com>
2015-12-18 10:48:47 -0500

    Update manifest documentation to reflect changes

--- src/device.ml
+++ src/device.ml
@@ -7,6 +7,7 @@ open Functional
 
 open Unsigned
 
+(** Identifying information for the device. *)
 module Info = struct
   type t = {
     id : word * word;
@@ -15,18 +16,34 @@ module Info = struct
   }
 end
 
+(** User input data that is necessary for devices to function. *)
 module Input = struct
+
+  (** Input state. *)
   type t = {
     key_code : uint8 option;
   }
 
+  (** The default (empty) input state. *)
   let none =
     { key_code = None; }
 end
 
+(** Common device interface. *)
 type t = <
+  (** Invoked the {! Engine} when the device receives an interrupt. *)
   on_interrupt : t Program.t IO.t;
+
+  (** Invoked by the {! Engine} on every iteraton of the event loop which
+      renders graphics and handles user input. *)
   on_interaction : Input.t -> Mem.t -> t IO.t;
+
+  (** Invoked the {! Engine} on every instruction that is decoded from memory
+      and executed.
+
+      The device can indicate here that it has generated an interrupt for the
+      processor. *)
   on_tick : (t * (Interrupt.t option)) IO.t;
+
   info : Info.t
 >
--- src/manifest.mli
+++ src/manifest.mli
@@ -10,28 +10,35 @@ type t
 
 exception No_such_device of device_index
 
+module Record : sig
+  type t = {
+    index : device_index;
+    device : Device.t
+  }
+end
+
 (** The empty manifest. *)
 val empty : t
 
 (** Register a new device in the manifest.
 
-    Registers devices are given an index that is unique to the manifest. *)
-val register : (module Device.S with type t = 'a) -> 'a -> t -> t
+    Registered devices are given an index that is unique to the manifest. *)
+val register : Device.t -> t -> t
 
 (** Update the record of an existing device.
 
-    If the device instance does not exist in the manifest, then the manifest is
+    If the record index does not exist in the manifest, then the manifest is
     unchanged. *)
-val update : (module Device.Instance) -> t -> t
+val update : Record.t -> t -> t
 
 (** Retrieve all registered devices. *)
-val all : t -> (module Device.Instance) list
+val all : t -> Record.t list
 
-(** Retrieve a device instance by its assigned index.
+(** Retrieve a manifest record by its assigned index.
 
     Assumes the provided index has been assigned. Raises {! No_such_device} if
     this is not the cause. *)
-val instance : device_index -> t -> (module Device.Instance)
+val get_record : device_index -> t -> Record.t
 
 (** Shortcut to query a device's information based on its assigned index. *)
 val query : device_index -> t -> Device.Info.t
